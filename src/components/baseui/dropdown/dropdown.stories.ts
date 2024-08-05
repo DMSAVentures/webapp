e2cae890b58ef1a9ddd055e7d12edd416359ec6e
protoappxyz <aars0001@agonquinlive.com>
2024-08-05 18:32:45 -0400

    some work for dropdown component

---
+++ src/components/baseui/dropdown/dropdown.stories.ts
@@ -0,0 +1,48 @@
+import Dropdown from "@/components/baseui/dropdown/dropdown";
+import {Meta, type StoryObj} from '@storybook/react';
+import { fn } from '@storybook/test';
+
+const meta: Meta = {
+    title: 'Components/Dropdown',
+    component: Dropdown,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+    argTypes: {
+        dropdownType: { control: 'text' },
+        size: { control: 'select', options: ['medium', 'small', 'x-small'] },
+        optional: { control: 'boolean' },
+        tooltip: { control: 'text' },
+        label: { control: 'text' },
+        hintText: { control: 'text' },
+        badge: { control: 'text' },
+        leftIcon: { control: 'text' },
+        placeholderText: { control: 'text' },
+    },
+    args: { onClick: fn() },
+} satisfies Meta<typeof Dropdown>;
+
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+    args: {
+        dropdownType: 'text',
+        size: 'medium',
+        optional: false,
+        tooltip: 'Tooltip',
+        label: 'Label',
+        hintText: 'Hint Text',
+        badge: 'Badge',
+        leftIcon: 'ri-question-line',
+        placeholderText: 'Placeholder Text',
+        options: [
+            {value: '', label: 'Placeholder Text'},
+            {value: '1', label: 'Option 1'},
+            {value: '2', label: 'Option 2'},
+            {value: '3', label: 'Option 3'}
+        ]
+    },
+};
---
+++ src/components/baseui/dropdown/dropdown.tsx
@@ -0,0 +1,84 @@
+import React, {EventHandler, HTMLAttributes, MouseEventHandler, useEffect, useRef, useState} from "react";
+import './dropdown.scss';
+import 'remixicon/fonts/remixicon.css';
+
+interface Option {
+    value: string;
+    label: string;
+}
+
+interface CustomSelectProps {
+    options: Option[];
+    placeholder: string;
+}
+
+interface DropdownProps extends HTMLAttributes<HTMLInputElement> {
+    dropdownType?: string;
+    size: 'medium' | 'small' | 'x-small';
+    optional?: boolean;
+    tooltip?: string;
+    label?: string;
+    hintText?: string;
+    badge?: string;
+    leftIcon?: string;
+    placeholderText?: string;
+    options: Option[];
+}
+
+const Dropdown: React.FC<DropdownProps> = (props) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [selectedOption, setSelectedOption] = useState<Option | null>(null);
+    const selectRef = useRef<HTMLDivElement>(null);
+
+    const toggleOpen = () => setIsOpen(!isOpen);
+
+    const handleOptionClick = (option: Option) => {
+        console.log(option);
+        setSelectedOption(option);
+        setIsOpen(false);
+    };
+
+    const handleClickOutside = (event: MouseEvent) => {
+        if (selectRef.current && !selectRef.current.contains(event.target as Node)) {
+            setIsOpen(false);
+        }
+
+    }
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+    console.log(selectedOption);
+    return (<div className={`dropdown dropdown--${props.size}`}>
+            <div className={'dropdown__label-container'}>
+                {props.label && <label className="dropdown__label">{props.label}</label>}
+                {props.optional && <label className="dropdown__optional">(Optional)</label>}
+                {props.badge && <span className="dropdown__badge">{props.badge}</span>}
+                {props.tooltip && <i className="dropdown__tooltip ri-information-line"/>}
+            </div>
+            <div className={`dropdown__select-container`} ref={selectRef}>
+                <div className={`dropdown__select`} onClick={toggleOpen}>
+                    {props.leftIcon && <i className={`dropdown__icon ${props.leftIcon}`}/>}
+                    <div className={'dropdown__select-text'}>
+                        {selectedOption ? selectedOption.label : props.placeholderText}
+                    </div>
+                    <i className="dropdown__icon ri-arrow-down-s-line"/>
+                </div>
+            </div>
+            {isOpen && (<div className="dropdown__options-container">
+                    {props.options.map((option) => (<span
+                            key={option.value}
+                            className={`dropdown-option ${selectedOption && selectedOption.value === option.value ? 'selected' : ''}`}
+                            onClick={() => handleOptionClick(option)}>
+                {option.label}
+              </span>))}
+                </div>)}
+            {props.hintText && <span className="dropdown__hint">{props.hintText}</span>}
+        </div>);
+};
+
+
+export default Dropdown;
