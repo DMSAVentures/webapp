d285c89c34e39cb1f3ba6f67c6cee54de5fa73f3
protoappxyz <aars0001@agonquinlive.com>
2024-08-11 15:33:19 -0400

    made a content card component

---
+++ src/components/baseui/label/contentcard.tsx
@@ -0,0 +1,39 @@
+import ContentLabel, {ContentLabelProps} from "@/components/baseui/label/contentlabel";
+import React from "react";
+import './contentcard.scss';
+import 'remixicon/fonts/remixicon.css';
+interface ContentCardProps extends ContentLabelProps {
+    dismissible? : boolean;
+    onDismiss? : () => void;
+}
+const ContentCard: React.FC<ContentCardProps> = (props) => {
+    const [dismissed, setDismissed] = React.useState(false);
+    if (dismissed) return null;
+
+    const onDismiss = () => {
+        if (props.onDismiss) {
+            props.onDismiss();
+        }
+        setDismissed(true);
+    }
+
+    return (
+        <div className={`content-card__container`}>
+            <ContentLabel
+                description={props.description}
+                text={props.text}
+                subText={props.subText}
+                badgeString={props.badgeString}
+                badgeColour={props.badgeColour}
+                disabled={props.disabled}
+                required={props.required}
+                imageSrc={props.imageSrc}
+                centeredImage={props.centeredImage}
+                linkTitle={props.linkTitle}
+                linkHref={props.linkHref}
+            />
+            {props.dismissible && <i className={'content-card__dismiss ri-close-fill'} onClick={onDismiss} />}
+        </div>
+    );
+}
+export default ContentCard;
