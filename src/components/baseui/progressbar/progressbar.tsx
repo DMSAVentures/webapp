1d96c2333edfd4edd88b0852f1a63fdcb8a7a014
protoappxyz <aars0001@agonquinlive.com>
2024-08-30 21:01:32 -0400

    Added progress bar line

---
+++ src/components/baseui/progressbar/progressbar.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './progressbar.scss';
+interface ProgressBarLineProps {
+    progress: number;
+    size: 'small' | 'medium' | 'large';
+    variant: 'success' | 'warning' | 'error' | 'info';
+    showPercentage: boolean;
+}
+
+const ProgressBarLine: React.FC<ProgressBarLineProps> = (props) => {
+    return (
+        <div className={`progress-bar`}>
+            <div className={`progress-bar__line progress-bar__line--${props.variant}`} style={{ width: `${props.progress}%` }}/>
+            {props.showPercentage && <span className="progress-bar__percentage">{props.progress}%</span>}
+        </div>
+    );
+}
+
+export default ProgressBarLine;
