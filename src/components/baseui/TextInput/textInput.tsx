9ffb0c613439782d3cb965a2d92820a81711db32
protoappxyz <aars0001@agonquinlive.com>
2024-09-08 17:53:02 -0400

    text input tweaks to include hint height

--- src/components/baseui/TextInput/textInput.tsx
+++ src/components/baseui/TextInput/textInput.tsx
@@ -16,16 +16,23 @@ export interface TextInputProps extends React.InputHTMLAttributes<HTMLInputEleme
 }
 
 export const TextInput = (props: TextInputProps) => {
+    const inputRef = React.createRef<HTMLInputElement>();
+
+    const handleFocus = () => {
+        inputRef.current?.focus();
+    }
     return (
         <div className={`text-input ${props.error ? 'text-input--error' : ''}`}>
             <Label text={props.label} required={props.required} />
-            <div className={`text-input__input-container ${props.error ? 'text-input__input-container--error' : ''} ${props.disabled ? 'text-input__input-container--disabled' : ''}`}>
+            <div className={`text-input__input-container ${props.error ? 'text-input__input-container--error' : ''} ${props.disabled ? 'text-input__input-container--disabled' : ''}`} onClick={handleFocus}>
                 {props.showLeftIcon && <i className={`text-input__input-container__icon text-input__input-container__icon--left ${props.leftIcon} `}/>}
-                <input {...props}/>
+                <input ref={inputRef} {...props}/>
                 {props.showRightIcon && <i className={`text-input__input-container__icon text-input__input-container__icon--right ${props.rightIcon} `}/>}
             </div>
-            {!props.error && props.hint && <HintText hintText={props.hint} state={props.error ? 'error' : 'default'}/>}
-            {props.error && <HintText hintText={props.error} state={props.error ? 'error' : 'default'}/>}
+            <div className={'text-input__hint'}>
+                {!props.error && props.hint && <HintText hintText={props.hint} state={props.error ? 'error' : 'default'}/>}
+                {props.error && <HintText hintText={props.error} state={props.error ? 'error' : 'default'}/>}
+            </div>
         </div>
     );
 }
