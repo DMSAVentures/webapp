810f8d3b0745a2f72c53b31711c9cf09117e1e0c
protoappxyz <aars0001@agonquinlive.com>
2024-10-26 19:10:10 -0400

    hide billing page behind suspense boundary

--- src/app/billing/page.tsx
+++ src/app/billing/page.tsx
@@ -1,20 +1,24 @@
 import Button from "@/components/baseui/button/button";
 import PlanCard from "@/components/billing/plans/planCard";
 import {getCurrentSubscription} from "@/hooks/getCurrentSubscription";
+import {Suspense} from "react";
+import LoadingSpinner from "@/components/loading/loadingSpinner";
 
 
 export default async function Page() {
     const data = await getCurrentSubscription();
 
     return (
-        <div>
-            <h1>Billing</h1>
-            <div>Subscription</div>
-            <PlanCard priceId={data.price_id!}/>
-            <p>Status: {data.status}</p>
-            <p>Next billing date: {data.end_date.toLocaleDateString()}</p>
-            <Button variant={'error'}>Cancel</Button>
-            <Button>Change Plan</Button>
-        </div>
+        <Suspense fallback={<LoadingSpinner/>}>
+            <div>
+                <h1>Billing</h1>
+                <div>Subscription</div>
+                <PlanCard priceId={data.price_id!}/>
+                <p>Status: {data.status}</p>
+                <p>Next billing date: {data.end_date.toLocaleDateString()}</p>
+                <Button variant={'error'}>Cancel</Button>
+                <Button>Change Plan</Button>
+            </div>
+        </Suspense>
     );
 }
