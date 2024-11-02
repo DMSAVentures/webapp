6ca41de691865abb1a2bf94d50e33b4decee8bdb
protoappxyz <aars0001@agonquinlive.com>
2024-11-02 18:37:18 -0400

    first simple ui component

---
+++ src/components/simpleui/Button/button.tsx
@@ -0,0 +1,37 @@
+import React, {FC, ButtonHTMLAttributes, useRef, useState, useEffect} from 'react';
+import './button.scss';
+
+// Define the possible variants for the button
+type ButtonVariant = 'primary' | 'secondary';
+
+// Interface for Button Props
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    variant?: ButtonVariant;
+    disabled?: boolean;
+}
+
+// Button component
+const Button: FC<ButtonProps> = ({
+                                     variant = 'primary',
+                                     disabled = false,
+                                     onClick,
+                                     children,
+                                     ...props
+                                 }) => {
+    const className = `button ${variant === 'secondary' ? 'button--secondary' : ''}`;
+
+    return (
+            <button
+                {...props} // Spread any additional props to the button
+                className={className}
+                disabled={disabled}
+                onClick={onClick}
+                aria-disabled={disabled}
+            >
+                {children}
+            </button>
+
+    );
+};
+
+export default Button;
