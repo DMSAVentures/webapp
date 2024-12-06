94438347fc86927dc59a47ba7fffdef737f14c2b
protoappxyz <aars0001@agonquinlive.com>
2024-12-06 02:00:08 -0500

    making side bar content extensible

--- src/components/simpleui/UIShell/Header/Header.tsx
+++ src/components/simpleui/UIShell/Header/Header.tsx
@@ -7,6 +7,7 @@ import {IconOnlyButton} from "@/components/simpleui/Button/IconOnlyButton";
 import {SidebarCollapsible} from "@/components/simpleui/UIShell/Sidebar/SidebarCollapsible";
 import {SidebarContent} from "@/components/simpleui/UIShell/Sidebar/sidebarContent";
 import {SidebarFooter} from "@/components/simpleui/UIShell/Sidebar/SidebarFooter";
+import {SidebarGroup} from "@/components/simpleui/UIShell/Sidebar/sidebarGroup";
 
 interface HeaderProps {
     logo: string;
@@ -28,7 +29,16 @@ const Header: React.FC<HeaderProps> = ({ logo, navItems, actionItems, toggleLeft
             {actionItems && <HeaderActions items={actionItems} />}
         </header>
             <SidebarCollapsible isOpen={isLeftNavOpen!} onClose={toggleLeftNav!}>
-                <SidebarContent/>
+                <SidebarContent>
+                    <SidebarGroup label="Main">
+                        <SidebarGroup.Item label="Dashboard" href="#dashboard" iconClass="dashboard-line" />
+                        <SidebarGroup.Item label="Reports" href="#reports" iconClass="file-chart-line" />
+                    </SidebarGroup>
+                    <SidebarGroup label="Settings">
+                        <SidebarGroup.Item label="Profile" href="#profile" iconClass="user-line" />
+                        <SidebarGroup.Item label="Security" href="#security" iconClass="shield-user-line" />
+                    </SidebarGroup>
+                </SidebarContent>
             </SidebarCollapsible>
         </div>
     );
--- src/components/simpleui/UIShell/Sidebar/sidebarContent.tsx
+++ src/components/simpleui/UIShell/Sidebar/sidebarContent.tsx
@@ -1,18 +1,13 @@
 import React from "react";
 import {SidebarGroup} from "@/components/simpleui/UIShell/Sidebar/sidebarGroup";
 import './sidebar-content.scss';
-
-export const SidebarContent: React.FC = () => {
+interface SidebarContentProps {
+    children: React.ReactElement<typeof SidebarGroup>[] | React.ReactElement<typeof SidebarGroup>;
+}
+export const SidebarContent: React.FC<SidebarContentProps> = (props: SidebarContentProps) => {
     return (
         <div className="sidebar__content">
-            <SidebarGroup label="Main">
-                <SidebarGroup.Item label="Dashboard" href="#dashboard" iconClass="dashboard-line" />
-                <SidebarGroup.Item label="Reports" href="#reports" iconClass="file-chart-line" />
-            </SidebarGroup>
-            <SidebarGroup label="Settings">
-                <SidebarGroup.Item label="Profile" href="#profile" iconClass="user-line" />
-                <SidebarGroup.Item label="Security" href="#security" iconClass="shield-user-line" />
-            </SidebarGroup>
+            {props.children}
         </div>
     );
 };
