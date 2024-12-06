7be69826b89a5265eeec7a78b2088e24cd51ce28
protoappxyz <aars0001@agonquinlive.com>
2024-12-05 22:59:09 -0500

    use new ui shell

--- src/app/web/layout.tsx
+++ src/app/web/layout.tsx
@@ -1,19 +1,14 @@
 import "./layout.scss";
 import React from "react";
-import Sidenav from "@/components/navigation/sidenav";
 import {Providers} from "@/contexts/providers";
+import {UIShellWithCollapsibleNavigation} from "@/components/simpleui/UIShell/Shell/UIShellWithCollapsibleNavigation";
 
 export default function Layout({children}: Readonly<{ children: React.ReactNode; }>) {
   return (
       <Providers>
-      <div className={"container"}>
-          <div className={"sidebar"}>
-              <Sidenav/>
-          </div>
-          <div className={"content"}>
-        {children}
-          </div>
-      </div>
+          <UIShellWithCollapsibleNavigation logo={"DMSA"}>
+              {children}
+          </UIShellWithCollapsibleNavigation>
       </Providers>
   );
 }
