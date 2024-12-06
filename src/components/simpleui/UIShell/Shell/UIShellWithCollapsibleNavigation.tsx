74cdc212e72f3b0ff2c0f5bf3dd4052e137da3fc
protoappxyz <aars0001@agonquinlive.com>
2024-12-05 22:53:21 -0500

    make ui shell take content as children

--- src/components/simpleui/UIShell/Shell/UIShellWithCollapsibleNavigation.tsx
+++ src/components/simpleui/UIShell/Shell/UIShellWithCollapsibleNavigation.tsx
@@ -6,22 +6,28 @@ import {SidebarCollapsible} from "@/components/simpleui/UIShell/Sidebar/SidebarC
 import {Sidebar} from "@/components/simpleui/UIShell/Sidebar/Sidebar";
 import './shell.scss';
 
-export const UIShellWithCollapsibleNavigation: React.FC = () => {
+interface UIShellWithCollapsibleNavigationProps {
+    children: React.ReactNode;
+    logo: string;
+}
+
+export const UIShellWithCollapsibleNavigation: React.FC<UIShellWithCollapsibleNavigationProps> = (props: UIShellWithCollapsibleNavigationProps) => {
     const [isLeftNavOpen, setIsLeftNavOpen] = React.useState(false);
     const toggleLeftNav = () => {
         setIsLeftNavOpen((prevState) => !prevState);
     };
     return (
         <div className="shell">
-            <Header logo={'DMSA'} isLeftNavOpen={isLeftNavOpen} toggleLeftNav={toggleLeftNav}/>
+            <Header logo={props.logo} isLeftNavOpen={isLeftNavOpen} toggleLeftNav={toggleLeftNav}/>
             <div className={'shell-container'}>
                 <div className="grid-wide">
-                    <Column sm={{span: 2, start: 7}}>
-                            Titles
-                    </Column>
-                    <Column sm={{span: 1, start: 4}}>
-                        Content
-                    </Column>
+                    {/*<Column sm={{span: 2, start: 7}}>*/}
+                    {/*        Titles*/}
+                    {/*</Column>*/}
+                    {/*<Column sm={{span: 1, start: 4}}>*/}
+                    {/*    Content*/}
+                    {/*</Column>*/}
+                    {props.children}
                 </div>
             </div>
         </div>
