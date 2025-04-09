24307ba0af31a3f0937b1f7c44c817d24b22a1dc
protoappxyz <shubh.aarsay@gmail.com>
2025-04-09 19:51:02 -0400

    Update url for endpoint

--- src/hooks/useSSEChat.ts
+++ src/hooks/useSSEChat.ts
@@ -34,7 +34,7 @@ export function useSSEChat() {
             body =  { message: input }
         }
 
-        const res = await fetch("http://localhost:8080/api/protected/ai/conversation", {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/protected/ai/conversation`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
