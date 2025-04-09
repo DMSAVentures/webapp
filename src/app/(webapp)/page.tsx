9c48cd361484584ee2326a4b513eaa018dd75b22
protoappxyz <shubh.aarsay@gmail.com>
2025-04-09 19:33:17 -0400

    Moving chat to homepage

--- src/app/(webapp)/page.tsx
+++ src/app/(webapp)/page.tsx
@@ -1,16 +1,14 @@
-import WebApp from "@/components/main/webapp";
+'use client';
 import {Column} from "@/components/simpleui/UIShell/Column/Column";
 import React from "react";
+import ChatBox from "@/components/ai/chatbox";
 
 export default function Home() {
     return (
         <>
 
-                <Column sm={{span: 4, start: 1}}>
-                    <WebApp/>
-                </Column>
-                <Column sm={{span: 1, start: 4}}>
-                    Content
+                <Column sm={{span: 8, start: 1}}>
+                    <ChatBox/>
                 </Column>
         </>
     );
--- src/hooks/useSSEChat.ts
+++ src/hooks/useSSEChat.ts
@@ -1,7 +1,5 @@
+'use client';
 import { useState } from "react"
-import {fetcher} from "@/hooks/fetcher";
-import {GetCurrentSubscriptionResponse} from "@/types/billing";
-
 
 type Message = {
     role: "user" | "assistant"
