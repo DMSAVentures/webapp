47605f9901d517ea81a1ce481c1a4d15c0136c7a
protoappxyz <me@shubhanshu.dev>
2025-04-17 12:57:39 -0400

    fix server component to client

--- src/routes/billing/plans.tsx
+++ src/routes/billing/plans.tsx
@@ -1,48 +1,49 @@
 import { createFileRoute } from '@tanstack/react-router'
 import PlanToPay from "@/components/billing/plans/planPay";
-import {getPlans} from "@/hooks/getPlans";
-import {Suspense} from "react";
 import LoadingSpinner from "@/components/loading/loadingSpinner";
 import {Column} from "@/components/simpleui/UIShell/Column/Column";
-
+import {useGetAllPrices} from "@/hooks/useGetAllPrices.ts";
+import {ErrorState} from "@/components/error/error.tsx";
 
 export const Route = createFileRoute('/billing/plans')({
   component: RouteComponent,
 })
 
+function Page() {
+    const {loading, prices, error} = useGetAllPrices()
 
+    if (loading) {
+        return <LoadingSpinner />;
+    }
 
+    if (error) {
+        return <ErrorState message={`Something went wrong: ${error}`} />;
+    }
 
-async function Page() {
-    const prices = await getPlans();  // This will trigger loading.tsx and error.tsx automatically if needed
-
-    if (!prices.length) {
+    if (!prices?.length) {
         return <div>No pricing plans available.</div>;
     }
 
     return (
-        <Suspense fallback={<LoadingSpinner/>}>
-            <>
-                <Column sm={{span: 7, start: 1}} >
-                    <h3>Plans</h3>
-                </Column>
-                <Column sm={{span: 7, start: 1}} md={{start: 1, span: 7}} lg={{start: 1, span: 11}}
-                        xlg={{start: 1, span: 13}}>
-                    <div className={'plans'}>
-                        {prices.map((price) => (
-                            <PlanToPay key={price.price_id} product_id={price.product_id} price_id={price.price_id}
-                                       description={price.description}/>
-                        ))}
-                    </div>
-                </Column>
-
-            </>
-        </Suspense>
+        <>
+            <Column sm={{ span: 7, start: 1 }}>
+                <h3>Plans</h3>
+            </Column>
+            <Column sm={{ span: 7, start: 1 }} md={{ start: 1, span: 7 }} lg={{ start: 1, span: 11 }}
+                xlg={{ start: 1, span: 13 }}>
+                <div className={'plans'}>
+                    {prices.map((price) => (
+                        <PlanToPay key={price.price_id} product_id={price.product_id} price_id={price.price_id}
+                            description={price.description} />
+                    ))}
+                </div>
+            </Column>
+        </>
     );
 }
 
-
 function RouteComponent() {
-  return <Page/>
+  return <Page />;
 }
+
 
