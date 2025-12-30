a88f6e8a3d691d42e2f48afd24c51cde5d462209
protoappxyz <me@shubhanshu.dev>
2025-12-30 12:02:00 -0500

    Delete checkbox with label

--- src/proto-design-system/components/forms/Checkbox/index.ts
+++ src/proto-design-system/components/forms/Checkbox/index.ts
@@ -1,8 +1,1 @@
 export { Checkbox, type CheckboxProps, type CheckboxSize } from "./Checkbox";
-export {
-  CheckboxWithLabel,
-  type CheckboxWithLabelProps,
-  type CheckboxWithLabelSize,
-  type CheckboxState,
-  type BadgeColour,
-} from "./CheckboxWithLabel";
